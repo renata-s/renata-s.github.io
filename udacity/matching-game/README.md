d1d06ecceb0ab4bd7ae309a582730d0c2a8d48bb
Renata Simaitiene <simaitiene.renata@gmail.com>
2018-03-30 16:05:36 +0300

    Add README.md file, fixed all issues after review.

---
+++ udacity/matching-game/README.md
@@ -0,0 +1,12 @@
+# Memory Game Project
+
+Matching game is the game that require players to match similar elements.
+
+![snippet](img/game.png)
+
+## Instructions
+
+* Click on a card this will start the game.
+* The object of the game is to turn over pairs of matching cards.
+* If a player succeeds in matching a pair, they have another turn and continue until they fail to match two cards. 
+* The object of the game is to match cards in less time with less moves.
--- udacity/matching-game/js/app.js
+++ udacity/matching-game/js/app.js
@@ -1,3 +1,10 @@
+/*jshint esversion: 6 */
+
+/**
+* @description Matching Game 
+* @constructor
+* @param {HtmlElement} containerEl - container element
+*/
 class MatchingGame {
     constructor(containerEl) {
         this.container = containerEl;
@@ -9,53 +16,66 @@ class MatchingGame {
         this.matchedCard = containerEl.getElementsByClassName('match');
         this.popup = containerEl.querySelector('.modal');
         this.popupBtn = containerEl.querySelector('.modal-btn');
-        this.popupBtn.addEventListener('click', this.playAgain.bind(this))
+        this.popupBtn.addEventListener('click', this.playAgain.bind(this));
         this.movesElement = containerEl.querySelector('.moves');
         this.moves = 0;
         this.timerStarted = false;
-        this.interval;
         this.stars = containerEl.querySelectorAll('.stars li');
         this.timer = containerEl.querySelector('.timer');
-        
+
         containerEl.querySelector('.restart').addEventListener('click', this.restart.bind(this));
         let game = this;
         this.cards.forEach(function (card) {
-            card.addEventListener('click', game.showCard.bind(game))
-            card.addEventListener('click', game.checkCards.bind(game))
-            card.addEventListener('click', game.checkForGameVictory.bind(game))
-        })
+            card.addEventListener('click', game.showCard.bind(game));
+            card.addEventListener('click', game.checkCards.bind(game));
+            card.addEventListener('click', game.checkForGameVictory.bind(game));
+        });
 
         this.restart();
     }
 
+    /**
+    * @description restart game 
+    */
     restart() {
+        // shuffle deck
         this.cards = this.shuffle(this.cards);
+        // remove all exisiting classes from each card
         this.deck.innerHTML = '';
         for (let i = 0; i < this.cards.length; i++) {
             let card = this.cards[i];
             card.classList.remove('show', 'open', 'match', 'disabled');
             this.deck.appendChild(card);
         }
+        // reset moves
         this.openedCards = [];
         this.movesElement.innerHTML = 0;
         this.moves = 0;
+        //reset timer
         this.timerStarted = false;
         this.timer.innerHTML = '0 mins 0 secs';
         clearInterval(this.interval);
         this.minute = 0;
         this.second = 0;
+        // reset star rating
         for (let i = 0; i < this.stars.length; i++) {
             this.stars[i].style.color = '#FFD700';
             this.stars[i].style.visibility = 'visible';
         }
     }
 
+    /**
+    * @description toggles open and show class to display cards
+    */
     showCard(el) {
         el.currentTarget.classList.toggle('open');
         el.currentTarget.classList.toggle('show');
         el.currentTarget.classList.toggle('disabled');
     }
 
+    /**
+    * @description add opened cards to openedCards list and check if cards are match or not
+    */
     checkCards(el) {
         if (!this.timerStarted) {
             this.timerStarted = true;
@@ -74,7 +94,11 @@ class MatchingGame {
         }
     }
 
-    // Shuffle function from http://stackoverflow.com/a/2450976
+    /**
+    * @description shuffles cards
+    * @param {array}
+    * @returns shuffledarray
+    */
     shuffle(array) {
         var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -89,10 +113,14 @@ class MatchingGame {
         return array;
     }
 
+    /**
+     * @description count player's moves
+    */
     incrementMoves() {
         this.moves = this.moves + 1;
         this.movesElement.innerHTML = this.moves;
 
+        // setting stars rating
         switch (this.moves) {
             case 8:
                 this.stars[2].style.color = 'black';
@@ -103,6 +131,9 @@ class MatchingGame {
         }
     }
 
+    /**
+     * @description for start timer
+    */
     startTimer() {
         let game = this;
         this.interval = setInterval(function () {
@@ -115,6 +146,9 @@ class MatchingGame {
         }, 1000);
     }
 
+    /**
+     * @description cards match
+    */
     matches() {
         this.openedCards[0].classList.add('match', 'disabled');
         this.openedCards[0].classList.remove('show', 'open', 'no-event');
@@ -123,6 +157,9 @@ class MatchingGame {
         this.openedCards = [];
     }
 
+    /**
+     * @description when cards unmatched
+    */
     unmatches() {
         this.openedCards[0].classList.add('unmatched');
         this.openedCards[1].classList.add('unmatched');
@@ -136,12 +173,18 @@ class MatchingGame {
         }, 1000);
     }
 
+    /**
+     * @description disable cards
+    */
     disable() {
         this.cards.forEach(function (card) {
             card.classList.add('disabled');
         });
     }
 
+    /**
+     * @description enable cards and disable matched cards
+    */
     enable() {
         let game = this;
         this.cards.forEach(function (card) {
@@ -152,6 +195,9 @@ class MatchingGame {
         });
     }
 
+    /**
+     * @description for game victory when all cards match, show modal
+     */
     checkForGameVictory() {
         if (this.matchedCard.length === this.cards.length) {
             clearInterval(this.interval);
@@ -159,6 +205,9 @@ class MatchingGame {
         }
     }
 
+    /**
+     * @description showing final stars, total moves and total time on modal
+     */
     congratulationsPopup() {
         this.popup.classList.toggle('show-modal');
         this.container.querySelector('#final-stars').innerHTML = this.container.querySelector('.stars').innerHTML;
@@ -166,13 +215,18 @@ class MatchingGame {
         this.container.querySelector('#totalMoves').innerHTML = this.moves;
     }
 
+    /**
+     * @desciption for user to play again
+    */
     playAgain() {
         this.popup.classList.toggle('show-modal');
         this.restart();
     }
-
 }
 
+/** 
+ * @desciption for load new game
+*/
 document.body.onload = function () {
-    let game = new MatchingGame(document.getElementById('matching-game'));
-}
+    (() => new MatchingGame(document.getElementById('matching-game')))();
+};
