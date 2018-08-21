e479be4980175efb7b3e321410e00ef5e0fc47f4
Renata Simaitiene <simaitiene.renata@gmail.com>
2018-08-21 21:08:47 +0300

    added map with Marker and InfoWindow

--- udacity/neighborhood-map-app/src/components/Map.js
+++ udacity/neighborhood-map-app/src/components/Map.js
@@ -1,22 +1,46 @@
-import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
-class Map extends Component {
-   render() {
-   const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
-        defaultZoom = { 13 }
-      >
-      </GoogleMap>
-   ));
-   return(
-      <div>
-        <GoogleMapExample
-          containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
-          mapElement={ <div style={{ height: `100%` }} /> }
-        />
-      </div>
-   );
-   }
-};
-export default Map;
+import React from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker,InfoWindow } from "react-google-maps"
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDfEhgi4c_HGJNjrKa4EMisx-l71acyiWY",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `800px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 54.896870, lng: 23.892429 }}
+  >
+      <Marker position={{ lat: 54.896870, lng: 23.892429 }} onClick={props.onMarkerClick}>
+      {props.isOpen && 
+      <InfoWindow onCloseClick={props.onInfoClose}>
+        <h4>Kaunas</h4>
+    </InfoWindow>
+      }
+      </Marker>
+  </GoogleMap>
+)
+
+export default class Map extends React.PureComponent {
+  state = {
+    isOpen: false
+  }
+
+  Toggle = () => {
+    this.setState({isOpen :!this.state.isOpen  })
+  }
+  render() {
+    return (
+      <MyMapComponent
+        isOpen={this.state.isOpen}
+        onMarkerClick={this.Toggle}
+        onInfoClose ={this.Toggle}
+      />
+    )
+  }
+}
