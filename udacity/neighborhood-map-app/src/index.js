286f518084d43ef3babc1f5cc9671f0e3f2564c9
Renata Simaitiene <simaitiene.renata@gmail.com>
2018-08-21 13:16:21 +0300

    Added Google Maps Api Created Map.js and .gitignore files

--- udacity/neighborhood-map-app/src/index.js
+++ udacity/neighborhood-map-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import './App.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App></App>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App></App>, document.getElementById('map'));
+//registerServiceWorker();
