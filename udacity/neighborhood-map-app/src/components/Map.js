8c5ffcc1ef07b5af5b9c432f2c28edbef33f1dfd
Renata Simaitiene <simaitiene.renata@gmail.com>
2018-08-23 16:10:45 +0300

    Added multiple markers and infowindows to the map

--- udacity/neighborhood-map-app/src/components/Map.js
+++ udacity/neighborhood-map-app/src/components/Map.js
@@ -2,6 +2,33 @@ import React from "react"
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker,InfoWindow } from "react-google-maps"
 
+
+class CustomMarker extends React.PureComponent {
+  state = {
+   isOpen: false
+  }
+
+  Toggle = () => {
+    this.setState({isOpen :!this.state.isOpen})
+  }
+  componentWillMount(){
+    this.setState({isOpen: this.props.isOpen});
+  }
+  render() {
+    
+    return (
+    <Marker key={this.props.i} position={{ lat: this.props.loc.location.lat, lng: this.props.loc.location.lng }} onClick={this.Toggle}>
+      { this.state.isOpen &&
+      <InfoWindow onCloseClick={this.Toggle}>
+          <h4>{this.props.loc.title}</h4>
+        </InfoWindow>
+      }
+    </Marker>
+    )
+   }
+  }
+
+
 const MyMapComponent = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDfEhgi4c_HGJNjrKa4EMisx-l71acyiWY",
@@ -13,22 +40,26 @@ const MyMapComponent = compose(
   withGoogleMap
 )((props) =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: 54.896870, lng: 23.892429 }}
+    defaultZoom={15}
+    defaultCenter={{ lat: 54.8989236, lng: 23.8854064 }}
   >
-      <Marker position={{ lat: 54.896870, lng: 23.892429 }} onClick={props.onMarkerClick}>
-      {props.isOpen && 
-      <InfoWindow onCloseClick={props.onInfoClose}>
-        <h4>Kaunas</h4>
-    </InfoWindow>
-      }
-      </Marker>
+  {
+     props.locations.map(function(loc, i){
+      return (<CustomMarker key={i} loc= {loc} i={i}></CustomMarker>)
+    })
+  }
   </GoogleMap>
 )
 
 export default class Map extends React.PureComponent {
   state = {
-    isOpen: false
+    locations : [
+      {title: 'Kaunas Castle', address: 'Pilies gatvė, Kaunas',  isOpen: false, location: {lat: 54.8989236, lng: 23.8854064}},
+      {title: 'Žalgirio Arena', address: 'Karaliaus Mindaugo prospektas, Kaunas',  isOpen:false, location: {lat: 54.8904589, lng: 23.9145647}},
+      {title: 'Oak-wood Park', address: 'Radvilėnų plentas, Kaunas', isOpen: false, location: {lat: 54.8995381, lng: 23.9457049}},
+      {title: 'KTU E-learning Technology Center', address: 'Studentų gatvė, Kaunas', isOpen: false, location: {lat: 54.9049311, lng: 23.9566562}},
+      {title: 'Flugplatz Aleksotas', address: 'Veiverių g., Kaunas', isOpen: false, location: {lat: 54.8775545, lng: 23.8821133}}
+    ]
   }
 
   Toggle = () => {
@@ -36,11 +67,7 @@ export default class Map extends React.PureComponent {
   }
   render() {
     return (
-      <MyMapComponent
-        isOpen={this.state.isOpen}
-        onMarkerClick={this.Toggle}
-        onInfoClose ={this.Toggle}
-      />
+      <MyMapComponent locations={this.state.locations} />
     )
   }
 }
